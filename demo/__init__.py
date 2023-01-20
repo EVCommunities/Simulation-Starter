bc163a41735fd8ec40b2b62eddef8d14ea080a65
Ville Heikkilä <ville.heikkila@tuni.fi>
2023-01-20 15:44:47 +0200

    Add code for loading parameters and outputting YAML

---
+++ demo/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Tampere University
+# This software was developed as a part of the EVCommunities project
+# This source code is licensed under the MIT license. See LICENSE in the repository root directory.
+# Author(s): Ville Heikkilä <ville.heikkila@tuni.fi>
